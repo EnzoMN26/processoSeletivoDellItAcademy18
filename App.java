68d3d616247a18b346dc5b1c9708109e20582b93
unknown <enzodrao@gmail.com>
2023-03-28 12:15:34 -0300

    corrigindo ortografia

--- App.java
+++ App.java
@@ -2,7 +2,7 @@ public class App {
     
     public static void main(String[] args){
         
-        //Intancia do menu.
+        //Instancia do menu.
         Menu menu = new Menu();
 
         //Chamada de metodo para inicio de execucao do menu.
